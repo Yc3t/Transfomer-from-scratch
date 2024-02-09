9255335e11b43efd388f8f072e0449067ec28096
Yc3t <ycetabi@gmail.com>
2024-02-09 12:30:48 +0100

    init function

--- model.py
+++ model.py
@@ -1,5 +1,9 @@
 import torch
-import torch.nn
+import torch.nn as nn
 
-Class InputEmbeddings(nn.Module):
-    
+class InputEmbeddings(nn.Module):
+    def __init__(self,d_model: int, vocab_size: int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size,d_model)
